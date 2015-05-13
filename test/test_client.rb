23b42fc20822b677b2843e66147a951945f86e3f
Greg Traub <greg.traub@gmail.com>
2015-05-13 16:32:45 -0400

    more tests, comments and namespace switching

--- test/test_client.rb
+++ test/test_client.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require '../idigbio/client'
+require '../lib/client'
 
 
 class TestClient < Minitest::Test
@@ -12,7 +12,13 @@ class TestClient < Minitest::Test
     end
 
     def test_record_search
-        assert @client.search_records(rq: {genus: 'puma'}).key?('items')
+        assert @client.search_records(rq: {genus: 'puma'}).key?('itemCount')
+    end
+
+    def test_record_search_with_block
+        @client.search_records(rq: {genus: 'puma'}) do |results|
+            assert results.key?('itemCount')
+        end
     end
 
     def test_media_search
@@ -21,5 +27,25 @@ class TestClient < Minitest::Test
 
     def test_view_record
         assert_equal 'd4ccd806-2fe5-49b3-af60-d4f1615a6fab', @client.view_record('d4ccd806-2fe5-49b3-af60-d4f1615a6fab')['uuid']
+    end
+
+    def test_view_media
+        assert_equal '2a4274aa-fd03-4753-9c69-3805de755516', @client.view_media('2a4274aa-fd03-4753-9c69-3805de755516')['uuid']
+    end
+
+    def test_view_recordset
+        assert_equal 'c2e06358-1f9f-463c-843f-446c0a37fbd0', @client.view_recordset('c2e06358-1f9f-463c-843f-446c0a37fbd0')['uuid']
+    end
+
+    def test_view_publisher
+        assert_equal '4e1beef9-d7c0-4ac0-87df-065bc5a55361', @client.view_publisher('4e1beef9-d7c0-4ac0-87df-065bc5a55361')['uuid']
+    end
+
+    def test_count_records
+        assert @client.count_records(rq: {genus: 'puma'}) >= 1 
+    end
+
+    def test_count_media
+        assert @client.count_media(rq: {genus: 'puma'}) >= 1 
     end
 end
